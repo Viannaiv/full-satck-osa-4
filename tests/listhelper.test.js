d877b39a0ce2f2553147dc45e9933858fe67e81f
Vivianna Tilander <viva95@windowslive.com>
2020-02-13 22:59:29 +0200

    Tehtävä 4.5*: apufunktioita ja yksikkötestejä, step3

--- tests/listhelper.test.js
+++ tests/listhelper.test.js
@@ -7,33 +7,50 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
+const listOfOneBlog = [{
+  _id: '1b432cv54y67k898995f22t7',
+  title: 'A test blog title',
+  author: 'a tester',
+  url: 'http://www.randomtestaddress.com/test',
+  likes: 3,
+  __v: 0
+}]
+
+const listOfBlogs = listOfOneBlog.concat([{
+  _id: '2b432cv54y67k898995f22t8',
+  title: 'A test blog title number 2',
+  author: 'a tester',
+  url: 'http://www.randomtestaddress.com/test2',
+  likes: 7,
+  __v: 0
+},
+{
+  _id: '3b432cv54y67k898995f22t9',
+  title: 'A test blog title number 3',
+  author: 'a tester',
+  url: 'http://www.randomtestaddress.com/test3',
+  likes: 5,
+  __v: 0
+},
+{
+  _id: '4b432cv54y67k898995f22t4',
+  title: 'A test blog title number 4',
+  author: 'a tester',
+  url: 'http://www.randomtestaddress.com/test4',
+  likes: 0,
+  __v: 0
+}])
+
+const listWithEqualLikes = listOfOneBlog.concat({
+  _id: '6b432cv54y67k898995f22t6',
+  title: 'A test blog title with identical likes',
+  author: 'a tester',
+  url: 'http://www.randomtestaddress.com/test',
+  likes: 3,
+  __v: 0
+})
+
 describe('total likes', () => {
-  const listOfOneBlog = [{
-    _id: '1b432cv54y67k898995f22t7',
-    title: 'A test blog title',
-    author: 'a tester',
-    url: 'http://www.randomtestaddress.com/test',
-    likes: 3,
-    __v: 0
-  }]
-
-  const listOfBlogs = listOfOneBlog.concat([{
-    _id: '2b432cv54y67k898995f22t8',
-    title: 'A test blog title number 2',
-    author: 'a tester',
-    url: 'http://www.randomtestaddress.com/test2',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '3b432cv54y67k898995f22t9',
-    title: 'A test blog title number 3',
-    author: 'a tester',
-    url: 'http://www.randomtestaddress.com/test3',
-    likes: 5,
-    __v: 0
-  }])
-
   test('of empty list of blogs is 0', () => {
     expect(listHelper.totalLikes([])).toBe(0)
   })
@@ -44,5 +61,34 @@ describe('total likes', () => {
 
   test('of a longer list is correctly calculated', () => {
     expect(listHelper.totalLikes(listOfBlogs)).toBe(15)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty array returns {}', () => {
+    expect(listHelper.favoriteBlog([])).toEqual({})
+  })
+
+  test('of a list containing one blog is the one blog', () => {
+    expect(listHelper.favoriteBlog(listOfOneBlog)).toEqual({
+      title: 'A test blog title',
+      author: 'a tester',
+      likes: 3,
+    })
+  })
+
+  test('of a longer list is the one with most likes', () => {
+    expect(listHelper.favoriteBlog(listOfBlogs)).toEqual({
+      title: 'A test blog title number 2',
+      author: 'a tester',
+      likes: 7,
+    })
+  })
+  test('of a list with equal highest likes returns the first of one', () => {
+    expect(listHelper.favoriteBlog(listWithEqualLikes)).toEqual({
+      title: 'A test blog title',
+      author: 'a tester',
+      likes: 3
+    })
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likelist = blogs.map(blog => blog.likes)
+  const blog = blogs.find(b => b.likes === Math.max(...likelist))
+
+  return blog
+    ? {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+    : {}
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
