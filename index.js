f58a5fb4800527bead57cad19d04f6569e9730d9
Vivianna Tilander <viva95@windowslive.com>
2020-02-13 19:49:44 +0200

    Tehtävä 4.1: blogilista, step1

---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+//const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.get('/api/blogs', (req, res) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      res.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then(result => {
+      res.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
